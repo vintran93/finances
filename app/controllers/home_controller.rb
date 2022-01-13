4a177be6a7f501b0e7c4f5336e4ea04ea903764d
Vincent Tran <vincentuong@yahoo.com>
2022-01-12 21:28:48 -0600

    search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -32,7 +32,6 @@ class HomeController < ApplicationController
       @response1 = Net::HTTP.get(@uri)
       @search_coin = JSON.parse(@response1)
       
-      @api = "https://financialmodelingprep.com/api/v3/stock/list?apikey=1d1c39289a199e97b82ab6d6f62bdabb"
       @api = 'https://financialmodelingprep.com/api/v3/stock/list?apikey=1d1c39289a199e97b82ab6d6f62bdabb'
       # 1d1c39289a199e97b82ab6d6f62bdabb
       @uri2 = URI(@api)
