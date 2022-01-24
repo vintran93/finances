4d114f78beb4fc4dafc8befff45b1732fce23b37
Vincent Tran <vincentuong@yahoo.com>
2022-01-24 17:14:15 -0600

    home index update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,22 +1,6 @@
 class HomeController < ApplicationController
 
     def index
-      #byebug
-      require 'net/http'
-      require 'json'
-      @url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=3faaac7a-4b4d-44d3-9001-1cb1e966e9ce'
-      @uri = URI(@url)
-      @response = Net::HTTP.get(@uri)
-      @coins = JSON.parse(@response)
-      #@api = 'https://financialmodelingprep.com/api/v3/stock/list?apikey=1d1c39289a199e97b82ab6d6f62bdabb'
-      #@api = "https://financialmodelingprep.com/api/v3/stock-screener?marketCapMoreThan=1000000000&betaMoreThan=1&volumeMoreThan=10000&sector=Technology&exchange=NASDAQ&dividendMoreThan=0&limit=100&apikey=1d1c39289a199e97b82ab6d6f62bdabb"
-      #@api = "https://financialmodelingprep.com/api/v3/quote-short/AAPL?apikey=1d1c39289a199e97b82ab6d6f62bdabb"
-      #@api = "https://financialmodelingprep.com/api/v3/profile/AAPL?apikey=1d1c39289a199e97b82ab6d6f62bdabb"
-      # 1d1c39289a199e97b82ab6d6f62bdabb
-      @api = "https://financialmodelingprep.com/api/v4/company-outlook?symbol=AAPL&apikey=1d1c39289a199e97b82ab6d6f62bdabb"
-      @uri2 = URI(@api)
-      @response2 = Net::HTTP.get(@uri2)
-      @search_stocks = JSON.parse(@response2)
     end
   
     def about
